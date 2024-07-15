7f826beded1fadaa4cc478990508759f6b404c7a
Aman Rajput <87659146+AmanRajput001@users.noreply.github.com>
2024-07-16 01:41:23 +0530

    React

---
+++ React_Learnings/Revision-1_UseState/src/App.jsx
@@ -0,0 +1,15 @@
+import Background from "./Background.jsx"
+import Foreground from "./Foreground.jsx"
+
+function App() {
+
+  return (
+    <>
+      <Background><Foreground></Foreground>
+      </Background>
+      
+    </>
+  )
+}
+
+export default App
---
+++ React_Learnings/Revision-1_UseState/src/Background.jsx
@@ -0,0 +1,11 @@
+import {useState} from 'react';
+
+function Background(){
+    return <>
+    <div className="z-0 position-absolute d-flex justify-content-center align-items-center bg-dark vw-100 vh-100">
+        <span className="text-secondary display-3 fw-bold">Learn With AR</span>
+    </div>
+    </>;
+}
+
+export default Background;
